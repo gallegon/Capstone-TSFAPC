155a99d672f29c02c727bd75b32cb8bf19f77ff5
MooseOnTheRocks <sam.foltz@hotmail.com>
2021-10-26 15:20:27 -0700

    las2img implemented mostly with numpy

--- las2img.py
+++ las2img.py
@@ -33,126 +33,69 @@ def main(file_path, resolution):
     min_xyz = (np.array([header.x_min, header.y_min, header.z_min]) / scale_xyz).astype("int")
     max_xyz = (np.array([header.x_max, header.y_max, header.z_max]) / scale_xyz).astype("int")
     range_xyz = (max_xyz - min_xyz) * scale_xyz
+    # I have the xyz position of each point
+    # points_xyz = [ x: [point_x...]
+    #              , y: [point_y...]
+    #              , z: [point_z...]
+    #              ]
+    points_xyz = np.array([data.X, data.Y, data.Z])
+
+    grid_width, grid_height = np.ceil(range_xyz[:2] / resolution).astype("int")
     print(f"Point count: {point_count}")
     print(f"X range: {range_xyz[0]} meters")
     print(f"Y range: {range_xyz[1]} meters")
     print(f"Z range: {range_xyz[2]} meters")
-    # I have the xyz position of each point
-    points_xyz = np.array([data.X, data.Y, data.Z])
+    print(f"Resolution: {resolution} meters per pixel")
+    print(f"Image dimensions: {grid_width}x{grid_height}")
 
     # I want the highest point within each cell
     # I'm going to store this as indices.
+    # grid_point_max_z_index will eventually contain the index
+    # for the point with the highest z value in each cell.
+    grid_point_max_z = np.full(shape=(grid_width, grid_height), fill_value=-1)
     cell_size_x, cell_size_y = resolution / scale_xyz[:2]
-    grid_width, grid_height = np.ceil(range_xyz[:2] / resolution).astype("int")
-    # print(cell_size_x, cell_size_y)
-    # print(grid_width, grid_height)
-    grid_indices = np.full(shape=(grid_width, grid_height), fill_value=min_xyz[2])
-    # For every point in the point cloud I need to:
-    # - Compute it's location in the grid_indices array
-    #   - By // cell_size
-    # - Update grid_indices to point to the point if it's z is greater than the current.
-
     # I need to find the point with the greatest z value within a cell
-    # I will do this by turning the xy coordinates into grid coordinates
-    cells_xy = ((points_xyz[:2, :] - min_xyz[:2].reshape((2, 1)))
-                // np.array([[cell_size_x], [cell_size_y]])).astype("int")
-    print(cells_xy)
-    # Find the tallest point in cell 0, 0
-    # mask_in_row_0 = cells_xy[0] == 0
-    # mask_in_col_0 = cells_xy[1] == 0
-    # mask_in_cell_0_0 = mask_in_row_0 & mask_in_col_0
-    # print(mask_in_row_0)
-    # print(mask_in_col_0)
-    # print(mask_in_cell_0_0)
+    # I will do this by turning the point xy coordinates into cell x coordinates
+    # For each point:
+    #       point_cell_x = (point_x - min_x) // cell_size_x
+    #       point_cell_y = (point_y - min_y) // cell_size_y
+    #
+    # points_xy_cell_index is an ndarray (2, point_count)
+    # points_xy_cell_index = [ x: [point_index...]
+    #                        , y: [point_index...]
+    #                        ]
+    points_xy_cell_index = (
+            (points_xyz[:2, :] - min_xyz[:2].reshape((2, 1)))
+            // np.array([[cell_size_x], [cell_size_y]])
+        ).astype("int")
+    
+    # points_in_cell = {(cell_x, cell_y): [indices of points in cell...]}
+    points_in_cell = {}
     points_indices = np.arange(point_count)
-    # points_z_in_cell_0_0 = points_xyz[2][mask_in_cell_0_0]
-    # indices_in_cell_0_0 = points_indices[mask_in_cell_0_0]
-    # print(indices_in_cell_0_0)
-    # print(points_z_in_cell_0_0)
-    # tallest_index_in_cell_0_0 = indices_in_cell_0_0[points_z_in_cell_0_0.argmax()]
-    # print(tallest_index_in_cell_0_0)
-    # tallest_point_in_cell_0_0 = points_xyz[:, tallest_index_in_cell_0_0]
-    # print(tallest_point_in_cell_0_0)
+    for cell_x in range(grid_width):
+        mask_in_cell_x = points_xy_cell_index[0] == cell_x
+        for cell_y in range(grid_height):
+            cell_xy = (cell_x, cell_y)
+            mask_in_cell_y = points_xy_cell_index[1] == cell_y
+            mask_in_cell_xy = mask_in_cell_x & mask_in_cell_y
+            points_in_this_cell = points_indices[mask_in_cell_xy]
+            points_in_cell[cell_xy] = points_in_this_cell
+            if len(points_in_this_cell) == 0:
+                continue
+            highest_z = points_xyz[2, points_in_this_cell].max()
+            grid_point_max_z[cell_x, cell_y] = highest_z
 
-    # Find the tallest point in every cell
-    # Store it in grid_indices[grid_x, grid_y] = tallest_point_index
-    # Create a mask for the tallest point in each row
-    # Create a mask for the tallest point in each column
-    # Output is the grid, where each element is an index to the highest point in that cell
-    # For each row, find the tallest point in the row
-    points_indices = np.arange(point_count)
-    mask_in_row_0 = cells_xy[0] == 0
-    # mask_in_cell_0_0 = mask_in_row_0 & mask_in_col_0
-    # print(mask_in_row_0)
-    # print(mask_in_col_0)
-    # print(mask_in_cell_0_0)
-    # points_z_in_cell_0_0 = points_xyz[2][mask_in_cell_0_0]
-    # indices_in_cell_0_0 = points_indices[mask_in_cell_0_0]
-    # print(indices_in_cell_0_0)
-    # print(points_z_in_cell_0_0)
-
-    # indices_in_cell_0_0 = points_indices[mask_in_cell_0_0]
-    # print(indices_in_cell_0_0)
-
-
-    print("done")
-
-    # I need to find the point with the greatest z value
-    # highest_z = points_xyz[2, :].argmax()
-    # print(points_xyz[:, highest_z])
-
-
-    # x_scale = data.header.scales[0]
-    # y_scale = data.header.scales[1]
-    # z_scale = data.header.scales[2]
-    # x_offset = data.header.offsets[0]
-    # y_offset = data.header.offsets[1]
-    # z_offset = data.header.offsets[2]
-    # # Keeping *_min, *_max, and *_range unscaled,
-    # # meaning they are all ints not floats.
-    # x_min = int(data.header.x_min / x_scale)
-    # x_max = int(data.header.x_max / x_scale)
-    # y_min = int(data.header.y_min / y_scale)
-    # y_max = int(data.header.y_max / y_scale)
-    # z_min = int(data.header.z_min / z_scale)
-    # z_max = int(data.header.z_max / z_scale)
-    # x_range = x_max - x_min
-    # y_range = y_max - y_min
-    # z_range = z_max - z_min
-    # print(f"X range: {x_range * x_scale} meters")
-    # print(f"Y range: {y_range * y_scale} meters")
-    # print(f"Z range: {z_range * z_scale} meters")
-    
-    # Using ceil to capture the very edges of the data as well.
-    # Integer rounding or flooring would leave off some information.
-    # Type sanity for dimension calculations:
-    # [x_range * scale] = meters
-    # [resolution] = meters / pixel
-    # meters / (meters / pixel) = meters * (pixels / meter) = pixels
-    # width_grid = ceil(x_range * x_scale / resolution)
-    # height_grid = ceil(y_range * y_scale / resolution)
-    # print(f"Resolution: {resolution} meters per pixel")
-    # print(f"Grid dimensions: {width_grid}x{height_grid} cells")
-    
-    # Creating a numpy 3 by point_count 2darray
-    # This will store the x, y, and z components
-    # Structure after the transpose is [point_index][x, y, z]
-    # points_xyz = np.array([data.X, data.Y, data.Z]).transpose()
-    # x_cell_size = resolution / x_scale
-    # y_cell_size = resolution / y_scale
-
-    
-    # image_grayscale = (grid - z_min) / (z_max - z_min)
-    # image_grayscale = (image_grayscale * 255).astype(np.uint8)
-    # img = Image.fromarray(image_grayscale, "L")
-    # # Format the name and save the image.
-    # save_path = "./rasters/"
-    # os.makedirs(save_path, exist_ok=True)
-    # file_name = os.path.split(file_path)[1]
-    # save_name = f"{file_name}_{resolution}-{width_grid}x{height_grid}.png"
-    # full_path = os.path.join(save_path, save_name)
-    # img.save(full_path)
-    # print(f"Saved image to \"{full_path}\"")
+    # Format the data into an image appropriate format for PIL.
+    image_grayscale = ((grid_point_max_z + 1) / max_xyz[2] * 255).astype("uint8").transpose()
+    img = Image.fromarray(image_grayscale, "L")
+    # Format the name and save the image.
+    save_path = "./rasters/"
+    os.makedirs(save_path, exist_ok=True)
+    file_name = os.path.split(file_path)[1]
+    save_name = f"{file_name}_{resolution}-{grid_width}x{grid_height}.png"
+    full_path = os.path.join(save_path, save_name)
+    img.save(full_path)
+    print(f"Saved image to \"{full_path}\"")
 
 
 if __name__ == "__main__":
