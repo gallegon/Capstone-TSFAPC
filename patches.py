c94f2eeacdcdc7b7f0d5ab05b9fb70325b7ec7d5
MooseOnTheRocks <sam.foltz@hotmail.com>
2021-11-05 22:22:35 -0700

    Added patches.py

---
+++ patches.py
@@ -0,0 +1,6 @@
+# patches.py
+#
+# Description:
+#   Transforms a LAS file into an aerial-view grayscale image.
+#   The lowest points are black, the highest points are white.
+#   Points for which there is no collected data are colored black.
