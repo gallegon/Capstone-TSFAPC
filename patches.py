a6e5e124c191b6bf66f959d75db3a0592f153657
MooseOnTheRocks <sam.foltz@hotmail.com>
2021-11-19 13:00:56 -0800

    Working with scipy.ndimage.find_objects

--- patches.py
+++ patches.py
@@ -5,22 +5,24 @@
 #   The lowest points are black, the highest points are white.
 #   Points for which there is no collected data are colored black.
 
-from scipy.ndimage import label, generate_binary_structure
+from scipy.ndimage import find_objects, generate_binary_structure
 
 from las2img import las2img
 
 if __name__ == "__main__":
     file_path = "./data/easy_nno.las"
-    resolution = 0.5
+    resolution = 10
     discretization = 32
     grid = las2img(file_path, resolution, discretization)
-
-    structure = [
-        [0, 1, 0],
-        [1, 1, 1],
-        [0, 1, 0]
-    ]
-    labels, num_labels = label(grid, structure)
-
-    print(f"Found {num_labels} labels!")
-    print(labels)
+    print("== Discretized Grid")
+    print(grid)
+    height_query = 24
+    print(f"== Query Height")
+    print(height_query)
+    containing_rect = find_objects(grid)[height_query - 1]
+    print("== Containing Rect")
+    print(containing_rect)
+    print(f"== Object label {height_query}")
+    print(grid[containing_rect] == height_query)
+    # print(f"Found {num_labels} labels!")
+    # print(labels)
