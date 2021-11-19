bf5e80835a05f34723cb469a912b27be26c8458d
MooseOnTheRocks <sam.foltz@hotmail.com>
2021-11-19 12:38:44 -0800

    Working with scipy.ndimage.label

--- patches.py
+++ patches.py
@@ -5,12 +5,22 @@
 #   The lowest points are black, the highest points are white.
 #   Points for which there is no collected data are colored black.
 
-from las2img import las2img
+from scipy.ndimage import label, generate_binary_structure
 
+from las2img import las2img
 
 if __name__ == "__main__":
     file_path = "./data/easy_nno.las"
     resolution = 0.5
     discretization = 32
     grid = las2img(file_path, resolution, discretization)
-    print(grid)
+
+    structure = [
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 1, 0]
+    ]
+    labels, num_labels = label(grid, structure)
+
+    print(f"Found {num_labels} labels!")
+    print(labels)
