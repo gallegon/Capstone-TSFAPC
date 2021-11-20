6e07a10fc2e1651fb364ab7fb78d09ea69563fd5
MooseOnTheRocks <sam.foltz@hotmail.com>
2021-11-19 22:18:39 -0800

    Cell coordinates per unique patch

--- patches.py
+++ patches.py
@@ -5,7 +5,8 @@
 #   The lowest points are black, the highest points are white.
 #   Points for which there is no collected data are colored black.
 
-from scipy.ndimage import find_objects, generate_binary_structure
+import numpy as np
+from scipy.ndimage import label, labeled_comprehension
 
 from las2img import las2img
 
@@ -13,16 +14,28 @@ if __name__ == "__main__":
     file_path = "./data/easy_nno.las"
     resolution = 10
     discretization = 32
+    
     grid = las2img(file_path, resolution, discretization)
     print("== Discretized Grid")
     print(grid)
+    
     height_query = 24
     print(f"== Query Height")
     print(height_query)
-    containing_rect = find_objects(grid)[height_query - 1]
-    print("== Containing Rect")
-    print(containing_rect)
-    print(f"== Object label {height_query}")
-    print(grid[containing_rect] == height_query)
-    # print(f"Found {num_labels} labels!")
-    # print(labels)
+    
+    objects = grid == height_query
+    neighbor_mask = [
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 1, 0]
+    ]
+    labels, num_labels = label(objects, neighbor_mask)
+    print("== Unique Patches")
+    print(f"Number of patches = {num_labels}")
+    print(labels)
+    
+    print("== Cell Coordinates for each Patch")
+    for i in range(1, num_labels + 1):
+        cells_xy = np.argwhere(labels == i)
+        print(f"-- Height {height_query}, Patch {i}")
+        print(cells_xy)
