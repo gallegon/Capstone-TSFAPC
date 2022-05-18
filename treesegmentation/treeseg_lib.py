8362d6f097029f9a55d86a3776320d10b73a96ad
Nicholai Gallegos <gallegon@oregonstate.edu>
2022-05-17 21:07:37 -0700

    added point cloud labeling, works in OSGeo4w shell

--- treesegmentation/treeseg_lib.py
+++ treesegmentation/treeseg_lib.py
@@ -1,7 +1,10 @@
-import laspy
 import os.path
 import timeit
 from PIL import Image
+import pdal
+import json
+import time
+import numpy as np
 
 from .hdag import *
 from .hierarchy import *
@@ -83,19 +86,43 @@ class Pipeline:
 
 
 def handle_read_las_data(input_file_path):
-    data = laspy.read(input_file_path)
-    header = data.header
-    point_count = header.point_count
-    scale_xyz = header.scales
-    # offset_xyz = header.offsets # Offsets currently not being used...
-    # Keeping the mins and maxs unscaled, making them ints and not floats.
-    # Doing this because laspy sample_data.X, .Y, and .Z are all unscaled ints.
-    # However *_min and *_max are all scaled.
-    min_xyz = (np.array([header.x_min, header.y_min, header.z_min]) / scale_xyz).astype("int")
-    max_xyz = (np.array([header.x_max, header.y_max, header.z_max]) / scale_xyz).astype("int")
+    # Construct and execute reader pipeline.
+    json_pipeline = [
+        {
+            "type": "readers.las",
+            "filename": input_file_path
+        }
+    ]
+    pdal_pipeline = pdal.Pipeline(json.dumps(json_pipeline))
+    pdal_pipeline.execute()
+
+    # Gather required data (metadata + point data) from processed .las file.
+    pdal_metadata = pdal_pipeline.metadata["metadata"]["readers.las"]
+    header_keys = [
+        "scale_x", "scale_y", "scale_z",
+        "offset_x", "offset_y", "offset_z",
+        "minx", "miny", "minz",
+        "maxx", "maxy", "maxz",
+        "count"
+    ]
+    header = {key: pdal_metadata[key] for key in header_keys}
+    pdal_data = pdal_pipeline.arrays[0]
+
+    # Structure the data from PDAL to be used by the rest of our program.
+    point_count = header["count"]
+    scale_xyz = np.array([header["scale_x"], header["scale_y"], header["scale_z"]])
+    min_xyz = (np.array([header["minx"], header["miny"], header["minz"]]) / scale_xyz).astype("int")
+    max_xyz = (np.array([header["maxx"], header["maxy"], header["maxz"]]) / scale_xyz).astype("int")
     bounds_xyz = (min_xyz, max_xyz)
     range_xyz = (max_xyz - min_xyz) * scale_xyz
-    points_xyz = np.array([data.X, data.Y, data.Z])
+
+    # Coordinates are represented using scaled floats.
+    # Want to keep arithmetic within integers for the algorithm.
+    xs = (pdal_data["X"] / scale_xyz[0]).astype("int")
+    ys = (pdal_data["Y"] / scale_xyz[2]).astype("int")
+    zs = (pdal_data["Z"] / scale_xyz[2]).astype("int")
+
+    points_xyz = np.array([xs, ys, zs])
 
     return {
         "points_xyz": points_xyz,
@@ -264,6 +291,10 @@ def handle_save_partition_raster(save_partition_raster, partition_raster_save_pa
 
     print(f"    - Saved partition raster to \"{full_path}\"")
 
+    return {
+        "png_raster_path": full_path
+    }
+
 
 def handle_save_patches_raster(save_patches_raster, patches_raster_save_path, labeled_grid, input_file_path, resolution, discretization):
     if not save_patches_raster:
@@ -330,6 +361,41 @@ def handle_save_centroids_raster(save_centroids_raster, centroids_raster_save_pa
     print(f"    - Saved centroids raster to \"{full_path}\"")
 
 
+def handle_label_point_cloud(input_file_path, bounds_xyz, scale_xyz, partition_raster_save_path, point_cloud_save_path, png_raster_path):
+    min_xyz, max_xyz = bounds_xyz
+    scale_x, scale_y, scale_z = scale_xyz
+
+    # min/max x/y for passing to gdal_translate
+    min_x = min_xyz[0] * scale_x
+    max_x = max_xyz[0] * scale_x
+    min_y = min_xyz[1] * scale_y
+    max_y = max_xyz[1] * scale_y
+
+    os.makedirs(point_cloud_save_path, exist_ok=True)
+
+    t = time.localtime()
+    current_time = time.strftime("%Y_%m_%d_%H%M%S", t)
+
+    # Output with date/time string appended
+    gtiff_output_path = os.path.join(partition_raster_save_path, f"output_{current_time}.tif")
+    las_output_path = os.path.join(point_cloud_save_path, f"output_{current_time}.las")
+
+    # spawn gdal_translate program to translate png to GeoTiff
+    translate_command = f"gdal_translate -of GTiff -a_srs EPSG:4326 -a_ullr {min_x} {min_y} {max_x} {max_y} {png_raster_path} {gtiff_output_path}"
+    os.system(translate_command)
+
+    print(f"saved gtiff to {gtiff_output_path}")
+
+    # Overlay the raster onto the point cloud using the pdal pipeline
+    pipeline = pdal.Reader.las(filename=input_file_path) | pdal.Filter.colorization(raster=gtiff_output_path, dimensions="Red, Green, Blue")
+    pipeline.execute()
+
+    # Write the colored/filtered points to the file
+    colored_points = pipeline.arrays[0]
+    pipeline = pdal.Writer.las(filename=las_output_path, dataformat_id=2, a_srs="EPSG:4326").pipeline(colored_points)
+    pipeline.execute()
+
+
 def print_runtime(f):
     def wrapper(*args, **kwargs):
         start = timeit.default_timer()
@@ -355,9 +421,7 @@ def run_algo(user_data):
         .then(handle_find_connected_hierarchies) \
         .then(handle_calculate_edge_weight) \
         .then(handle_partition_graph) \
-        .then(handle_partitions_to_labeled_grid) \
-        .then(handle_adjust_partitions) \
-        .then(handle_partitions_to_trees)
+        .then(handle_trees_to_labeled_grid)
 
     # handle_save_labeled_grid_as_image has an if checking for should_save as well,
     # so having both ifs is redundant. Doing this to show that there is a lot of
@@ -367,6 +431,7 @@ def run_algo(user_data):
         .then(handle_label_points) \
         .then(handle_save_tree_raster)
 
+    algorithm.then(handle_label_point_cloud)
     algorithm.intersperse(print_runtime)
 
     result = algorithm.execute(user_data)
