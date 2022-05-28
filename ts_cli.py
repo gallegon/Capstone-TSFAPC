90a5c96a0289bac88391e63f5cd2ef59f4dc7568
MooseOnTheRocks <sam.foltz@hotmail.com>
2022-05-27 17:42:55 -0700

    Saved directory restructuring, default paramaters for pipeline handlers

--- ts_cli.py
+++ ts_cli.py
@@ -1,28 +1,33 @@
 from treesegmentation.treeseg_lib import *
-from treesegmentation.laslabel import laslabel
-from timeit import default_timer
 
 
 def main():
-    level_depth_weight = 0.84
-    node_depth_weight = 1.07
-    shared_ratio_weight = -0.11
-    top_distance_weight = 0.77
-    centroid_distance_weight = 0.0
-    weights = np.array([level_depth_weight, node_depth_weight,
-                        shared_ratio_weight, top_distance_weight,
-                        centroid_distance_weight], dtype=np.float32)
+    weight_level_depth = 0.84
+    weight_node_depth = 1.07
+    weight_shared_ratio = -0.11
+    weight_top_distance = 0.77
+    weight_centroid_distance = 0.0
 
     user_data = {
         "input_file_path": "sample_data/hard_nno.las",
-        "weights": weights,
+        "output_folder_name": None,
+        "save_folder_name": None,
+
+        "weight_level_depth": weight_level_depth,
+        "weight_node_depth": weight_node_depth,
+        "weight_shared_ratio": weight_shared_ratio,
+        "weight_top_distance": weight_top_distance,
+        "weight_centroid_distance": weight_centroid_distance,
         "weight_threshold": 0.7,
+
         "resolution": 0.6,
         "discretization": 64,
         "min_height": 10,
         "neighbor_mask": NEIGHBOR_MASK_FOUR_WAY,
         "gaussian": False,
         "gaussian_sigma": 0.1,
+        "espg_string": "EPSG:2027",
+
         "save_grid_raster": True,
         "grid_raster_save_path": "grid_rasters",
         "save_patches_raster": True,
@@ -34,10 +39,10 @@ def main():
         "save_tree_raster": False,
         "tree_raster_save_path": "tree_rasters",
         "point_cloud_save_path": "labeled_point_clouds",
-        "espg_string": "EPSG:2027"
     }
 
-    algorithm = Pipeline() \
+    algorithm = Pipeline(verbose=True) \
+        .then(handle_create_file_names_and_paths) \
         .then(handle_read_las_data) \
         .then(handle_las2img) \
         .then(handle_gaussian_filter) \
@@ -48,32 +53,20 @@ def main():
         .then(handle_compute_patch_neighbors) \
         .then(handle_save_patches_raster) \
         .then(handle_compute_hierarchies) \
-        .then(handle_save_centroids_raster) \
         .then(handle_find_connected_hierarchies) \
         .then(handle_calculate_edge_weight) \
         .then(handle_partition_graph) \
-        .then(handle_trees_to_labeled_grid)
-
-    #print(user_data)
-    # handle_save_labeled_grid_as_image has an if checking for should_save as well,
-    # so having both ifs is redundant. Doing this to show that there is a lot of
-    # flexibility in how the Pipeline and its components are used.
-    if user_data["save_partition_raster"]:
-        algorithm.then(handle_save_partition_raster) \
+        .then(handle_trees_to_labeled_grid) \
+        .then(handle_save_partition_raster) \
         .then(handle_label_points) \
-        .then(handle_save_tree_raster)
+        .then(handle_label_point_cloud) \
+        .then(handle_save_context_file)
 
-    algorithm.then(handle_label_point_cloud)
-    algorithm.intersperse(print_runtime)
+    if algorithm.verbose:
+        algorithm.intersperse(transform_print_runtime)
 
     result = algorithm.execute(user_data)
-    print("== Result")
-    print(result.keys())
-    # Commented out for testing
-    '''
-    print("== Tree Count")
-    print(len(result["partitioned_graph"]))
-    '''
+    return result
 
 
 if __name__ == "__main__":
